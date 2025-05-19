9d0432f4f40e9b99d9e33ac3c4934276286f5b5b
AngelYQ1 <angel.yucra.q@tecsup.edu.pe>
2025-05-18 21:34:37 -0500

    Configurar instancias únicas

--- app/src/main/java/com/example/lab08/TaskDatabase.kt
+++ app/src/main/java/com/example/lab08/TaskDatabase.kt
@@ -1,10 +1,27 @@
 package com.example.lab08
-
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import android.content.Context
 
-@Database(entities = [Task::class], version = 1)
+@Database(entities = [Task::class], version = 2)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
-}
 
+    companion object {
+        @Volatile
+        private var instance: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase {
+            return instance ?: synchronized(this) {
+                val newInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_db"
+                ).fallbackToDestructiveMigration().build()
+                instance = newInstance
+                newInstance
+            }
+        }
+    }
+}
